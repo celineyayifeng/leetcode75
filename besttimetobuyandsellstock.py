e8093390dd23c56b1f76e298b655040e54e1bbd9
Celine Feng <83599921+celineyayifeng@users.noreply.github.com>
2022-03-20 20:49:16 -0400

    Update besttimetobuyandsellstock.py

--- besttimetobuyandsellstock.py
+++ besttimetobuyandsellstock.py
@@ -17,3 +17,38 @@ class Solution(object):
             if old_profit < new_profit: 
                 old_profit = new_profit 
         return old_profit 
+
+solution #2 
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_buy = prices[0]
+        old_profit = 0
+        new_profit = 0
+        for i in range(1,len(prices)):
+            if prices[i] > min_buy: 
+                new_profit = max(old_profit, prices[i]-min_buy)
+                old_profit = new_profit 
+            else:
+                min_buy = prices[i]
+        return new_profit 
+    
+Solution #3
+
+ class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        loss= prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            loss = min(loss, prices[i])
+            profit = max(profit, prices[i]-loss)
+        return profit
