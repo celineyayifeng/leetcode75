83da93676b75714f590d7d111218ae1901ac37b7
Celine Feng <83599921+celineyayifeng@users.noreply.github.com>
2026-03-08 12:47:16 -0400

    Refactor lengthOfLongestSubstring with type hints
    Refactor lengthOfLongestSubstring method to use type hints and improve readability.

--- longestsubstringwithoutrepeat.py
+++ longestsubstringwithoutrepeat.py
@@ -1,21 +1,18 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        charset = set()
-        left = 0
-        res = 0
-        for right in range(len(s)):
-            while s[right] in charset: #right window starts move
-                # print(s[right])
-                charset.remove(s[left]) # filter duplicate with set and shrink window
-                left += 1 # left window moves
-                
-            charset.add(s[right])
-            res = max(res, right - left + 1) #range of farthest right and left window 
-        return res
+class Solution: 
+    def lengthOfLongestSubstring(self, s: str) -> int: 
+        # two pointer sliding window  
+        left = 0 # pointer of sliding window
+        max_length = 0 # value to return
+        charset = set() # keep current characters forming the longest string 
+
+        for right in range(len(s)): 
+            while s[right] in charset: # if value already exists
+                charset.remove(s[left]) # remove the value that already exists 
+                left += 1 # move left pointer 
+            charset.add(s[right]) 
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
     
 # time complexity: O(n)
 # space complexity: O(n)
